47ef11414819bfffb6168b95e97eb4b83fff5795
vladyslav-kurmaz <posolkoronovan@gmail.com>
2022-11-22 18:59:15 +0200

    Added theability to automatically create classes in card elements

--- js/script.js
+++ js/script.js
@@ -288,7 +288,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // карточки меню   
 
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.title = title;
       this.descr = descr;
       this.price = price;
@@ -296,6 +296,7 @@ window.addEventListener('DOMContentLoaded', () => {
       this.alt = alt;
       this.parent = document.querySelector(parentSelector);
       this.changer = 40;
+      this.classes = classes;
       this.changeToUAH();
     
     }
@@ -306,16 +307,22 @@ window.addEventListener('DOMContentLoaded', () => {
 
     render() {
       const element = document.createElement('div');
-      element.innerHTML += `<div class="menu__item">
-                                <img src=${this.src} alt=${this.alt}>
-                                <h3 class="menu__item-subtitle">Меню ${this.title}</h3>
-                                <div class="menu__item-descr">${this.descr}</div>
-                                <div class="menu__item-divider"></div>
-                                <div class="menu__item-price">
-                                    <div class="menu__item-cost">Цена:</div>
-                                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                                </div>
-                            </div>`;
+
+      if (this.classes.length === 0) {
+        this.element = 'menu__item';
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
+
+      element.innerHTML += `  <img src=${this.src} alt=${this.alt}>
+                              <h3 class="menu__item-subtitle">Меню ${this.title}</h3>
+                              <div class="menu__item-descr">${this.descr}</div>
+                              <div class="menu__item-divider"></div>
+                              <div class="menu__item-price">
+                                  <div class="menu__item-cost">Цена:</div>
+                                  <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                              </div>`;
 
         this.parent.append(element);
     }
@@ -329,7 +336,8 @@ window.addEventListener('DOMContentLoaded', () => {
                        Продукт активных и здоровых людей. Это абсолютно новый продукт 
                        с оптимальной ценой и высоким качеством!`,
                        6,
-                       '.menu .container'
+                       '.menu .container',
+                       
  ).render();
 
   new MenuCard(
@@ -340,7 +348,8 @@ window.addEventListener('DOMContentLoaded', () => {
                     но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты 
                     - ресторанное меню без похода в ресторан!`,
                     16,
-                    '.menu .container' 
+                    '.menu .container',
+                    
   ).render();
 
   new MenuCard(
@@ -351,7 +360,9 @@ window.addEventListener('DOMContentLoaded', () => {
                      животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество 
                      белков за счет тофу и импортных вегетарианских стейков.`, 
                     10.8, 
-                    '.menu .container'
+                    '.menu .container',
+                    'menu__item',
+                    
   ).render();
 
 });
